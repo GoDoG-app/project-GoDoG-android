8941f082e82156110d9e540e1a86013dbae3203c
the9world <130967390+the9world@users.noreply.github.com>
2023-09-11 14:57:01 +0900

    walking3 지도2 구현(거리,시간 못구함, 방법 변경예정)

--- app/src/main/java/com/blue/walking/Walking3.java
+++ app/src/main/java/com/blue/walking/Walking3.java
@@ -15,12 +15,21 @@ import android.widget.TextView;
 import com.blue.walking.config.Config;
 import com.blue.walking.model.Park;
 import com.blue.walking.model.UserInfo;
+import com.google.android.gms.maps.model.LatLng;
 import com.skt.tmap.TMapData;
 import com.skt.tmap.TMapPoint;
 import com.skt.tmap.TMapView;
 import com.skt.tmap.overlay.TMapPolyLine;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Walking3 extends AppCompatActivity {
 
@@ -28,18 +37,24 @@ public class Walking3 extends AppCompatActivity {
     ImageView imgLoop;  // 산책로 새로고침
     TextView txtRegion;  // 현재 위치 주소
 
-    FrameLayout mapView1;   // 첫번째 산책로 지도
-    TextView txtPlace1; // 첫번재 산책로 이름
+    FrameLayout mapView1; // 1번 산책로 지도
+    TextView txtPlace1; // 1번 산책지 이름
+    TextView txtRoute1; // 1번 산책지 첫 번째 경로 이름
     TextView txtTime1;  // 첫번째 산책로 이동 시간
     TextView txtKm1;    // 첫번째 산책로 거리
 
     FrameLayout mapView2;   // 두번째 산책로 지도
-    TextView txtPlace2; // 두번째 산책로 이름
+    TextView txtPlace2; // 2번 산책지 이름
+    TextView txtRoute2; // 2번 산책지 두 번째 경로 이름
     TextView txtTime2;  // 두번째 산책로 이동 시간
     TextView txtKm2;    // 두번째 산책로 거리
 
-    Park park;
-    UserInfo userInfo;
+    Park park; // 유저가 선택한 공원 정보
+    UserInfo userInfo; // 유저의 정보
+    double startLat; // 유저의 출발지
+    double startLng;
+    double endLat; // 유저의 도착지
+    double endLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +75,46 @@ public class Walking3 extends AppCompatActivity {
             // userInfo가 null인 경우 처리
             Log.e("UserInfo", "UserInfo null");
         }
+        startLat = userInfo.lat; // 출발지
+        startLng = userInfo.lng;
+
+        endLat = park.getLatitude(); // 도착지
+        endLng = park.getLongitude();
+
+        TMapData tMapData = new TMapData(); // 경로를 그릴 데이터 생성
+        TMapPoint startPoint = new TMapPoint(startLat, startLng); // 출발지
+        TMapPoint endPoint =  new TMapPoint(endLat, endLng); // 도착지
+        TMapPoint middlePoint = getMidPoint(startPoint, endPoint); // 중간 지점 계산
+
+
+        // 랜덤 포인트를 ArrayList(경유지)에 넣기
+        ArrayList<TMapPoint> passList = new ArrayList<>();
+        double randomRadius = 0.0015; // 반경 설정 (임의로 조정)
+        double randomAngle = Math.toRadians(new Random().nextDouble() * 360.0);
+        double randomLatOffset = randomRadius * Math.cos(randomAngle);
+        double randomLngOffset = randomRadius * Math.sin(randomAngle);
+        TMapPoint randomPoint = new TMapPoint(endLat + randomLatOffset, endLng + randomLngOffset);
+        passList.add(endPoint); // 경유지에 유저가 선택한 공원
+        passList.add(randomPoint); // 랜덤 포인트 가까운거리
+
 
 
         mapView1 = findViewById(R.id.mapView1);
         mapView2 = findViewById(R.id.mapView2);
+        txtRegion = findViewById(R.id.txtRegion);
+        txtPlace1 = findViewById(R.id.txtPlace1);
+        txtPlace2 = findViewById(R.id.txtPlace2);
+        txtRoute1 = findViewById(R.id.txtRoute1);
+        txtRoute2 = findViewById(R.id.txtRoute2);
+        txtTime1 = findViewById(R.id.txtTime1);
+        txtTime2 = findViewById(R.id.txtTime2);
+        txtKm1 = findViewById(R.id.txtKm1);
+        txtKm2 = findViewById(R.id.txtKm2);
+        txtRegion.setText(userInfo.userAddress);
+        txtPlace1.setText(park.getName()+"(빠른 경로)");
+        txtPlace2.setText(park.getName()+"(느린 경로)");
 
-        // 유저가 선택한 경로 정보 intent 받기
+
 
 
         // 경로 추천용 두개의 지도
@@ -89,59 +138,113 @@ public class Walking3 extends AppCompatActivity {
             public void onMapReady() {
                 // todo 맵이 준비 되었다면 여기서 작성
 
-                // 에러 방지용 더미 변수
-//                double startLat = 37.5455; // 출발지
-//                double startLng = 126.6758;
-//
-//                double endLat = 37.5475394; // 도착지
-//                double endLng = 126.6665509;
-
-//        passList.add(new TMapPoint( 37.5455, 126.6758)); // 경유지 서구청
-//        passList.add(new TMapPoint( 37.5475394, 126.6665509)); // 경유지 아시아드
-
-                double startLat = userInfo.lat; // 출발지
-                double startLng = userInfo.lng;
-
-                double endLat = park.getLatitude(); // 도착지
-                double endLng = park.getLongitude();
-
-
-                // 추천 경로 그리기 1번
-                ArrayList<TMapPoint> passList = new ArrayList<>();
-                TMapData tMapData1 = new TMapData();
-                TMapPoint startPoint = new TMapPoint(startLat, startLng); // 출발지
-                TMapPoint endPoint =  new TMapPoint(endLat, endLng); // 도착지
-
                 // searchOption string 0: 추천 (기본값), 4: 추천+대로우선, 10: 최단, 30: 최단거리+계단제외
-                tMapData1.findPathDataWithType(TMapData.TMapPathType.PEDESTRIAN_PATH,
-                        startPoint, endPoint, null, 0, new TMapData.OnFindPathDataWithTypeListener() {
+                tMapData.findPathDataWithType(TMapData.TMapPathType.PEDESTRIAN_PATH,
+                        startPoint, startPoint, passList, 0, new TMapData.OnFindPathDataWithTypeListener() {
                             @Override
                             public void onFindPathDataWithType(TMapPolyLine tMapPolyLine) {
-//                                // 중간 지점 계산
-                                TMapPoint middlePoint = getMidPoint(startPoint, endPoint);
 
                                 // 중간 지점을 지도의 중심으로 설정.
                                 tMapView1.setCenterPoint(middlePoint.getLatitude(), middlePoint.getLongitude());
-                                tMapView1.setZoomLevel(17); // 줌 레벨 설정
+                                tMapView1.setZoomLevel(15); // 줌 레벨 설정
 
                                 tMapPolyLine.setLineColor(Color.DKGRAY); // 경로 폴리라인 색상
                                 tMapPolyLine.setLineWidth(3); // 경로 폴리라인 두께
                                 tMapView1.addTMapPolyLine(tMapPolyLine);
+
+                                double distance = tMapPolyLine.getDistance(); // 총 거리 (미터)
+                                Log.i("거리", "distance" + distance);
+                                double time = tMapPolyLine.getDistance() / 1000.0 / 5.0; // 예상 시간 (시간, 평균 보행 속도 5km/h 기준)
+
+                                // UI 업데이트를 메인 스레드에서 실행
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        txtTime1.setText(String.valueOf(time));
+                                        txtKm1.setText(String.valueOf(distance/1000.0));
+                                    }
+                                });
+
                             }
                         });
-
-//                TMapPoint midPoint = startPoint.get // 중간 지점 계산
-
             }
         });
 
+        // 랜덤 포인트 생성 (중간 지점 주변)
+        ArrayList<TMapPoint> passList1 = new ArrayList<>();
+        double randomRadius1 = 0.0030; // 반경 설정 (임의로 조정)
+        double randomAngle1 = Math.toRadians(new Random().nextDouble() * 360.0);
+        double randomLatOffset1 = randomRadius1 * Math.cos(randomAngle1);
+        double randomLngOffset1 = randomRadius1 * Math.sin(randomAngle1);
+        TMapPoint randomPoint1 = new TMapPoint(startLat + randomLatOffset1, startLng + randomLngOffset1);
+        passList1.add(endPoint); // 경유지에 유저가 선택한 공원
+        passList1.add(randomPoint1); // 랜덤포인트(경유지) 추가 먼거리
 
         tMapView2.setOnMapReadyListener(new TMapView.OnMapReadyListener() {
             @Override
             public void onMapReady() {
+                // 추천 경로 그리기 2번
 
-            }
+                // searchOption string 0: 추천 (기본값), 4: 추천+대로우선, 10: 최단, 30: 최단거리+계단제외
+                tMapData.findPathDataWithType(TMapData.TMapPathType.PEDESTRIAN_PATH,
+                        startPoint, startPoint, passList1, 0, new TMapData.OnFindPathDataWithTypeListener() {
+                            @Override
+                            public void onFindPathDataWithType(TMapPolyLine tMapPolyLine) {
+                                // 중간 지점 계산
+                                TMapPoint middlePoint = getMidPoint(startPoint, endPoint);
+
+                                // 중간 지점을 지도의 중심으로 설정.
+                                tMapView2.setCenterPoint(middlePoint.getLatitude(), middlePoint.getLongitude());
+                                tMapView2.setZoomLevel(15); // 줌 레벨 설정
+
+                                tMapPolyLine.setLineColor(Color.DKGRAY); // 경로 폴리라인 색상
+                                tMapPolyLine.setLineWidth(3); // 경로 폴리라인 두께
+                                tMapView2.addTMapPolyLine(tMapPolyLine);
+
+                                double distance = tMapPolyLine.getDistance(); // 총 거리 (미터)
+                                double time = tMapPolyLine.getDistance() / 1000.0 / 5.0; // 예상 시간 (시간, 평균 보행 속도 5km/h 기준)
+
+                                // UI 업데이트를 메인 스레드에서 실행
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        txtTime2.setText(String.valueOf(time));
+                                        txtKm2.setText(String.valueOf(distance));
+                                    }
+                                });
+
+                            }
+                });
+           }
         });
+
+//        OkHttpClient client = new OkHttpClient();
+//
+//        MediaType mediaType = MediaType.parse("application/json");
+//        RequestBody body = RequestBody.create(mediaType,
+//                "{\"startX\":126.92365493654832," +
+//                        "\"startY\":37.556770374096615," +
+//                        "\"angle\":20," +
+//                        "\"speed\":30," +
+//                        "\"endPoiId\":\"10001\"," +
+//                        "\"endX\":126.92432158129688," +
+//                        "\"endY\":37.55279861528311," +
+//                        "\"passList\":\"126.92774822,37.55395475_126.92577620,37.55337145\",\"reqCoordType\":\"WGS84GEO\",\"startName\":\"%EC%B6%9C%EB%B0%9C\",\"endName\":\"%EB%8F%84%EC%B0%A9\",\"searchOption\":\"0\",\"resCoordType\":\"WGS84GEO\",\"sort\":\"index\"}");
+//        Request request = new Request.Builder()
+//                .url("https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&callback=function")
+//                .post(body)
+//                .addHeader("accept", "application/json")
+//                .addHeader("content-type", "application/json")
+//                .addHeader("appKey", Config.getAppKey())
+//                .build();
+//
+//        try {
+//            Response response = client.newCall(request).execute();
+//
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
         mapView1.setOnClickListener(new View.OnClickListener() {
             @Override
